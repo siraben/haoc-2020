80f99c469824c7e8f0fe6f1f881ca115380cd92d
Ben Siraphob <bensiraphob@gmail.com>
2020-12-03 13:33:25 +0700

    day3: shorten!

--- day3.hs
+++ day3.hs
@@ -13,11 +13,11 @@ solveGeneric r = fst . foldl' f (0, r) . tail
     f (hs, cc) l = (hs + if l `T.index` cc == '#' then 1 else 0, (cc + r) `mod` 31)
 
 part2 :: [T.Text] -> Int
-part2 i = product ((`solveGeneric` i) <$> [1, 3, 5, 7]) * foo
+part2 i = product ((`solveGeneric` i) <$> [1, 3, 5, 7]) * r1d2
   where
-    foo = fst (foldl' g (0, 1) (tail (fst (foldr f ([], True) i))))
-    f x (l, b) = (if b then x : l else l, not b)
-    g (hs, cc) l = (hs + if l `T.index` cc == '#' then 1 else 0, (cc + 1) `mod` 31)
+    -- Alternate lines and accumulate result at the same time
+    r1d2 = let (a, _, _) = foldl' g (0, 1, False) (tail i) in a
+    g (hs, cc, b) l = (hs + if b && l `T.index` cc == '#' then 1 else 0, (cc + if b then 1 else 0) `mod` 31, not b)
 
 main = do
   inp <- T.lines <$> TIO.readFile "day3.txt"
