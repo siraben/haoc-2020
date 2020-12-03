a214e1613bf2a1ec89b4f8f1aafb2f6030225f86
Ben Siraphob <bensiraphob@gmail.com>
2020-12-03 14:28:47 +0700

    day3: bang patterns

--- day3.hs
+++ day3.hs
@@ -1,3 +1,4 @@
+{-# LANGUAGE BangPatterns #-}
 import Data.List (foldl')
 import qualified Data.ByteString.Char8 as B
 import Criterion.Main
@@ -6,8 +7,8 @@ import Criterion.Main
 solve i r d = let (a,_,_) = foldl' f (0, r, d) i in a
   where
     -- (number of #s, col counter, row counter)
-    f (hs, cc, 0) l = (hs + if l `B.index` cc == '#' then 1 else 0, (cc + r) `mod` 31, d - 1)
-    f (hs, cc, dc) _ = (hs, cc, dc - 1)
+    f (!hs, !cc, 0) l = (hs + if l `B.index` cc == '#' then 1 else 0, (cc + r) `mod` 31, d - 1)
+    f (!hs, !cc, !dc) _ = (hs, cc, dc - 1)
 
 part1, part2 :: [B.ByteString] -> Int
 part1 i = solve i 3 1
