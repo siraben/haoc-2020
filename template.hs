12dd381c5b8486e26a78130d26cc5bb95af57473
Ben Siraphob <bensiraphob@gmail.com>
2020-12-04 20:18:23 +0700

    Add more libraries to template

--- template.hs
+++ template.hs
@@ -1,6 +1,11 @@
-{-# LANGUAGE BangPatterns #-}
+{-# LANGUAGE BangPatterns, OverloadedStrings #-}
 import Criterion.Main
 import qualified Data.ByteString.Char8 as B
+import qualified Data.IntSet as IS
+import qualified Data.Map as M
+import qualified Data.Set as S
+import qualified Data.Text as T
+import qualified Data.Text.IO as TIO
 
 part1, part2 :: [B.ByteString] -> Int
 part1 i = undefined
@@ -14,5 +19,5 @@ main = do
   print (part1 inp)
   print (part2 inp)
   defaultMain [ bgroup dayString [ bench "part1" $ whnf part1 inp
-                              , bench "part2" $ whnf part2 inp
-                              ] ]
+                                 , bench "part2" $ whnf part2 inp
+                                 ] ]
