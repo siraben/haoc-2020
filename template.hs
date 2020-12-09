be07c46de30fa49b99de2f6ac18921b79ca30e70
Ben Siraphob <bensiraphob@gmail.com>
2020-12-09 15:13:46 +0700

    template: expand with more functions

--- template.hs
+++ template.hs
@@ -2,23 +2,34 @@
 {-# LANGUAGE FlexibleInstances #-}
 {-# LANGUAGE OverloadedStrings #-}
 {-# LANGUAGE PartialTypeSignatures #-}
+{-# LANGUAGE TupleSections #-}
 {-# LANGUAGE TypeApplications #-}
 {-# LANGUAGE NoMonomorphismRestriction #-}
-{-# OPTIONS_GHC -Wno-partial-type-signatures -fdefer-typed-holes #-}
+{-# OPTIONS_GHC -Wno-partial-type-signatures -fdefer-typed-holes -fno-warn-unused-imports #-}
 
 import Criterion.Main
+import Data.Bifunctor
 import Data.Bits
+import Data.ByteString.Char8 (ByteString)
 import qualified Data.ByteString.Char8 as B
 import Data.Char
+import Data.Foldable
 import Data.Function
 import qualified Data.Graph as G
+import Data.IntSet (IntSet)
 import qualified Data.IntSet as IS
 import Data.List
+import Data.Map (Map)
 import qualified Data.Map as M
 import Data.Maybe
+import Data.Set (Set)
 import qualified Data.Set as S
+import Data.Text (Text)
 import qualified Data.Text as T
 import qualified Data.Text.IO as TIO
+import Data.Vector (Vector)
+import qualified Data.Vector as V
+import Control.Monad
 
 -- Slow splitOn for prototyping
 splitOn :: String -> String -> [String]
@@ -35,16 +46,66 @@ splitOn' del bs = go bs
           then ls : mempty
           else ls : splitOn' del (B.drop n rest)
 
-countIf f = length . filter f
+-- Useful functions
+findFirst :: Foldable f => (a -> Bool) -> f a -> a
 findFirst f = fromJust . find f
 
 slidingWindows :: Int -> [Int] -> [[Int]]
 slidingWindows n l = take n <$> tails l
 
+-- | Compute the average of the elements of a container.
+avg :: (Integral c, Foldable t) => t c -> c
+avg = uncurry div . foldl' (\(s, l) x -> (x + s, succ l)) (0, 0)
+
+swap (a, b) = (b, a)
+
+dup a = (a, a)
+
+-- | Count the number of items in a container where the predicate is true.
+countTrue :: Foldable f => (a -> Bool) -> f a -> Int
+countTrue p = length . filter p . toList
+
+-- | Build a frequency map
+freqs :: (Foldable f, Ord a) => f a -> Map a Int
+freqs = M.fromListWith (+) . map (,1) . toList
+
+-- | map a function over elems satisfying a predicate
+mapIf p f = foldl' (\xs x -> if p x then f x : xs else x : xs) []
+
+-- | Set the element at index @n@ to @x@
+setAt n x = (\(l, r) -> l ++ x : tail r) . splitAt n
+
+-- | Like @findIndices@ but also return the element found for each index
+findIndicesElem :: Foldable t => (a -> Bool) -> t a -> [(a, Int)]
+findIndicesElem p = fst . foldl' go ([], 0 :: Int)
+  where
+    go (l, n) x
+      | p x = ((x, n) : l, n + 1)
+      | otherwise = (l, n + 1)
+
+-- | Perturb a list's elements satisfying a predicate with a function
+pertubationsBy :: (a -> Bool) -> (a -> a) -> [a] -> [[a]]
+pertubationsBy p f l = [setAt n (f x) l | (x, n) <- findIndicesElem p l]
+
+-- | Unconditional pertubation
+pertubations :: (a -> a) -> [a] -> [[a]]
+pertubations = pertubationsBy (const True)
+
+
+-- | Repeat a function until you get the same result twice.
+fixedPoint :: Eq a => (a -> a) -> a -> a
+fixedPoint f = go
+  where
+    go !x
+        | x == y    = x
+        | otherwise = go y
+      where
+        y = f x
+
+-- Start working down here
 part1, part2 :: _ -> Int
 part1 i = undefined
 part2 i = undefined
-
 main = do
   let dayNumber = undefined :: Int
   let dayString = "day" <> show dayNumber
