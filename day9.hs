6292651f369080e4f6f4e25a15a2ecdcc784f930
Ben Siraphob <bensiraphob@gmail.com>
2020-12-09 13:15:46 +0700

    day9: reindent

--- day9.hs
+++ day9.hs
@@ -1,17 +1,16 @@
 {-# LANGUAGE BangPatterns #-}
 {-# LANGUAGE OverloadedStrings #-}
 
+import Criterion.Main
 import Data.List
 import Data.Maybe
-import Criterion.Main
-import qualified Data.IntSet as IS
 import qualified Data.Vector as V
--- subl a b list = take (b - a) (drop a list)
 
 part1 :: [Int] -> Int
-part1 inp' = head (snd (fromJust (find (\(p,r) -> isNotValid p (head r)) (splitAt 25 <$> tails inp'))))
+part1 inp' = head (snd (fromJust (find (\(p, r) -> isNotValid p (head r)) (splitAt 25 <$> tails inp'))))
   where
-    isNotValid p n = null [(x,y) | x <- p, y <- p, x + y == n]
+    isNotValid p n = null [(x, y) | x <- p, y <- p, x + y == n]
+
 {-
   (\l -> minimum l + maximum l) (fromJust (find ((== target) . sum) (segs inp')))
 = head (map (\l -> minimum l + maximum l) (filter ((== target) . sum) (segs inp')))
@@ -20,15 +19,13 @@ part1 inp' = head (snd (fromJust (find (\(p,r) -> isNotValid p (head r)) (splitA
 = head (foldr (\x y -> if sum x == target then (minimum x + maximum x):y else y) [] (concat (map tails (inits inp'))))
 -- seems stuck
 -}
--- part2 :: Int -> [Int] -> Int
+part2 :: Int -> V.Vector Int -> Int
 part2 target inp = minimum l + maximum l
   where
     inp' = V.scanl (+) 0 inp
     l = V.slice a (b - a) inp
     n = length inp'
-    (a,b) = head [(i,j) | i <- [0..n-1], j <- [i..n - 1], inp' V.! j - inp' V.! i == target]
-
-findFirst f = fromJust . find f
+    (a, b) = head [(i, j) | i <- [0 .. n - 1], j <- [i .. n - 1], inp' V.! j - inp' V.! i == target]
 
 main = do
   let dayNumber = 9
