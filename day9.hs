0a36b301cb04874d2f88241d54e31e6137be5b77
Ben Siraphob <bensiraphob@gmail.com>
2020-12-09 12:12:04 +0700

    day9: solve

--- day9.hs
+++ day9.hs
@@ -6,7 +6,7 @@
 {-# LANGUAGE NoMonomorphismRestriction #-}
 {-# OPTIONS_GHC -Wno-partial-type-signatures -fdefer-typed-holes #-}
 
-import Criterion.Main
+-- import Criterion.Main
 import Data.Bits
 import qualified Data.ByteString.Char8 as B
 import Data.Char
@@ -38,17 +38,33 @@ part1, part2 :: _ -> Int
 part1 i = undefined
 part2 i = undefined
 
+-- 27911108
+subl a b l = [l !! i | i <- [a..b]]
+
 main = do
-  let dayNumber = undefined :: Int
+  let dayNumber = 9
   let dayString = "day" <> show dayNumber
   let dayFilename = dayString <> ".txt"
   inp <- lines <$> readFile dayFilename
-  print (part1 inp)
-  print (part2 inp)
-  defaultMain
-    [ bgroup
-        dayString
-        [ bench "part1" $ whnf part1 inp,
-          bench "part2" $ whnf part2 inp
-        ]
-    ]
+  let inp' = read <$> inp :: [Int]
+  let bbb = iterate (drop 1) inp'
+  let bing l = splitAt 25 l
+  -- let aaa = drop 25 inp'
+  let isValid p n = not (null [(x,y) | x <- p, y <- p, x + y == n])
+  -- print inp'
+  let x = (filter (not . (\(p,r) -> isValid p (head r))) (bing <$> bbb))
+  print (head (snd (head x)))
+  let n = length inp'
+  let segs = concatMap tails . inits
+  print ((\l -> minimum l + maximum l) <$> (filter ((== 27911108) . sum) (segs inp')))
+  -- print [(a,b) | a <- [0..length inp - 1], b <- [a..length inp - 1], let n = subl a b inp', sum n == 27911108]
+  --- print (head (filter (not . isValid)  aaa))
+  -- print (part1 inp)
+  -- print (part2 inp)
+  -- defaultMain
+  --   [ bgroup
+  --       dayString
+  --       [ bench "part1" $ whnf part1 inp,
+  --         bench "part2" $ whnf part2 inp
+  --       ]
+  --   ]
