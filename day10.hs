4cde9f7b5989d45ed02e66991cd3f5d694f6da4e
Ben Siraphob <bensiraphob@gmail.com>
2020-12-10 13:27:30 +0700

    day10: cleanup

--- day10.hs
+++ day10.hs
@@ -2,47 +2,27 @@
 
 import Criterion.Main
 import Data.Foldable
-import Data.Function
 import qualified Data.IntMap as IM
 import qualified Data.IntSet as IS
 import Data.List
-import qualified Data.Map as M
-import qualified Data.Set as S
 
-valid a b = (b - a) <= 3
-
-countL [] = 0
--- bad exponential time solution
--- perhaps drop b
-countL (a : b : c : rest) = re + dr
-  where
-    -- drop a
-    re = countL (b : c : rest)
-    -- drop b if a->c still valid
-    dr = if valid a c then countL (a : c : rest) else 0
-countL [a, b] = 1
-countL [a] = 1
-
--- f is the function to "fix" over in a weird way
-foo target f = fix (\x -> f (M.fromSet x keys M.!))
-  where
-    keys = S.fromList [0 .. target]
-
--- | Count the number of items in a container where the predicate is true.
-countTrue :: Foldable f => (a -> Bool) -> f a -> Int
-countTrue p = length . filter p . toList
-
-part1 inp' = countTrue (3 ==) ds * countTrue (1 ==) ds
+part1 :: [Int] -> Int
+part1 inp' = uncurry (*) (foldl' f (0,0) ds)
   where
     ds = zipWith (-) (tail inp') inp'
+    f (a,b) 1 = (a+1,b)
+    f (a,b) 3 = (a,b+1)
 
+part2 :: [Int] -> Int
 part2 = findElseZero 0 . pathsToGoal . IS.fromList
 
+findElseZero :: Int -> IM.IntMap Int -> Int
 findElseZero = IM.findWithDefault 0
 
 pathsToGoal :: IS.IntSet -> IM.IntMap Int
 pathsToGoal is = res
   where
+    -- define an IntMap in terms of itself, yay lazy evaluation!
     res = (`IM.fromSet` is) $ \i ->
       if i == goal
         then 1
