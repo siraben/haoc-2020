d3e5d93dbab1f42ee2a3a6e457b19fb38a15770b
Ben Siraphob <bensiraphob@gmail.com>
2020-12-19 12:57:06 +0700

    day19: cleanup

--- day19.hs
+++ day19.hs
@@ -1,130 +1,19 @@
-{-# LANGUAGE BangPatterns #-}
-{-# LANGUAGE FlexibleContexts #-}
-{-# LANGUAGE FlexibleInstances #-}
-{-# LANGUAGE LambdaCase #-}
-{-# LANGUAGE OverloadedStrings #-}
-{-# LANGUAGE PartialTypeSignatures #-}
-{-# LANGUAGE TupleSections #-}
-{-# LANGUAGE TypeApplications #-}
-{-# LANGUAGE NoMonomorphismRestriction #-}
-{-# OPTIONS_GHC -Wno-partial-type-signatures -fdefer-typed-holes -fno-warn-unused-imports #-}
-
-import Control.Monad
-import Criterion.Main
-import Data.Bifunctor
-import Data.Bits
-import Data.ByteString.Char8 (ByteString)
-import qualified Data.ByteString.Char8 as B
-import Data.Char
+import Data.Either
 import Data.Foldable
-import Data.Function
 import Data.Functor
-import qualified Data.Graph as G
 import Data.IntMap (IntMap)
 import qualified Data.IntMap as IM
-import Data.IntSet (IntSet)
-import qualified Data.IntSet as IS
-import Data.Ix
 import Data.List
-import Data.Map (Map)
-import qualified Data.Map as M
-import Data.Maybe
-import Data.Set (Set)
-import qualified Data.Set as S
-import Data.Text (Text)
-import qualified Data.Text as T
-import qualified Data.Text.IO as TIO
-import Data.Vector (Vector)
-import qualified Data.Vector as V
 import Text.ParserCombinators.Parsec
-import Data.Either
-
--- Slow splitOn for prototyping
-splitOn :: String -> String -> [String]
-splitOn sep s = T.unpack <$> T.splitOn (T.pack sep) (T.pack s)
-
--- ByteString splitOn
-splitOn' :: B.ByteString -> B.ByteString -> [B.ByteString]
-splitOn' del bs = go bs
-  where
-    n = B.length del
-    go bs = case B.breakSubstring del bs of
-      (ls, rest) ->
-        if B.null rest
-          then ls : mempty
-          else ls : splitOn' del (B.drop n rest)
-
--- Useful functions
-findFirst :: Foldable f => (a -> Bool) -> f a -> a
-findFirst f = fromJust . find f
-
-slidingWindows :: Int -> [Int] -> [[Int]]
-slidingWindows n l = take n <$> tails l
-
--- | Compute the average of the elements of a container.
-avg :: (Integral c, Foldable t) => t c -> c
-avg = uncurry div . foldl' (\(s, l) x -> (x + s, succ l)) (0, 0)
-
-swap (a, b) = (b, a)
-
-dup a = (a, a)
 
 -- | Count the number of items in a container where the predicate is true.
 countTrue :: Foldable f => (a -> Bool) -> f a -> Int
 countTrue p = length . filter p . toList
 
--- | Build a frequency map
-freqs :: (Foldable f, Ord a) => f a -> Map a Int
-freqs = M.fromListWith (+) . map (,1) . toList
-
--- | map a function over elems satisfying a predicate
-mapIf p f = foldl' (\xs x -> if p x then f x : xs else x : xs) []
-
--- | Set the element at index @n@ to @x@
-setAt n x = (\(l, r) -> l ++ x : tail r) . splitAt n
-
--- | Like @findIndices@ but also return the element found for each index
-findIndicesElem :: Foldable t => (a -> Bool) -> t a -> [(a, Int)]
-findIndicesElem p = fst . foldl' go ([], 0 :: Int)
-  where
-    go (l, n) x
-      | p x = ((x, n) : l, n + 1)
-      | otherwise = (l, n + 1)
-
--- | Perturb a list's elements satisfying a predicate with a function
-pertubationsBy :: (a -> Bool) -> (a -> a) -> [a] -> [[a]]
-pertubationsBy p f l = [setAt n (f x) l | (x, n) <- findIndicesElem p l]
-
--- | Unconditional pertubation
-pertubations :: (a -> a) -> [a] -> [[a]]
-pertubations = pertubationsBy (const True)
-
--- | Generate all the segments of a list, O(n^2)
-segs :: [a] -> [[a]]
-segs = concatMap tails . inits
-
--- | Repeat a function until you get the same result twice.
-fixedPoint :: Eq a => (a -> a) -> a -> a
-fixedPoint f = go
-  where
-    go !x
-      | x == y = x
-      | otherwise = go y
-      where
-        y = f x
-
--- Start working down here
-part1, part2 :: _ -> Int
-part1 i = undefined
-part2 i = undefined
-
 data RuleD = Seq [Int] | Alt [Int] [Int] | Lit String
   deriving (Show, Eq)
 
-type Rule = (Int,RuleD)
-
-parens :: Parser a -> Parser a
-parens p = string "(" *> p <* string ")"
+type Rule = (Int, RuleD)
 
 tok :: Parser a -> Parser a
 tok p = p <* spaces
@@ -170,7 +59,6 @@ foo (i, b) e = case b of
     )
   Seq l -> (i, foldl1' (*>) [p | u <- [(i, e IM.! i) | i <- l], let (_, p) = foo u e])
 
-
 main = do
   let dayNumber = 19
   let dayString = "day" <> show dayNumber
@@ -181,7 +69,11 @@ main = do
   let Right inp' = traverse pp ps
   print inp'
   let inp'' = IM.fromList inp' :: Env
-  print (countTrue isRight ((parse ((snd (foo (0, inp'' IM.! 0) inp'')) <* eof) "") <$> inps))
+  -- print (countTrue isRight ((parse ((snd (foo (0, inp'' IM.! 0) inp'')) <* eof) "") <$> inps))
+  let rule0 = snd (foo (0, inp'' IM.! 0) inp'') <* eof
+  -- print (parse rule0 "" "bbbbbbbaaaabbbbaaabbabaaa")
+  -- mapM_ print (filter (isRight . parse rule0 "") inps)
+  print (countTrue isRight (parse rule0 "" <$> inps))
 
 -- print (part1 inp)
 -- print (part2 inp)
