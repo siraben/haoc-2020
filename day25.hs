08243eb384ca389e7b8b52248281e96ae49e9529
Ben Siraphob <bensiraphob@gmail.com>
2020-12-25 12:50:37 +0700

    day25: benchmark

--- day25.hs
+++ day25.hs
@@ -1,6 +1,6 @@
 {-# LANGUAGE BangPatterns #-}
 
--- import Criterion.Main
+import Criterion.Main
 import Data.Foldable
 import Data.Maybe
 
@@ -14,28 +14,29 @@ iter f = go
     go 0 x = x
     go n x = go (pred n) $! f x
 
-step (!v,!s) = ((v*s) `mod` 20201227,s)
+step (!v, !s) = ((v * s) `mod` 20201227, s)
 
--- transform :: Integer -> Integer -> Integer
-transform loopSize subjectNum = fst (iter step loopSize (1,subjectNum))
--- transform loopSize subjectNum = (subjectNum ^ loopSize) `mod` 20201227
+transform :: Int -> Int -> Int
+transform loopSize subjectNum = fst (iter step loopSize (1, subjectNum))
 
-crack k l = fst (findFirst ((== l). snd) (zip [1..] (iterate ((`mod` 20201227) . (* k) :: Int -> Int) 7)))
+crack k l = fst (findFirst ((== l) . snd) (zip [1 ..] (iterate ((`mod` 20201227) . (* k) :: Int -> Int) 7)))
+
+part1 (a,b) = (crack 7 a, crack 7 b)
+part2 ((a,b),(cls,dls)) = (transform cls b, transform dls a)
 
 main = do
   let dayNumber = 25
   let dayString = "day" <> show dayNumber
   let dayFilename = dayString <> ".txt"
-  [a,b] <- map read . lines <$> readFile dayFilename :: IO [Int]
-  -- let inp = (a,b)
-  let cardLoopSize = crack 7 a
-  let doorLoopSize = crack 7 b
-  print (cardLoopSize, doorLoopSize)
-  print (transform cardLoopSize b, transform doorLoopSize a)
-  -- defaultMain
-  --   [ bgroup
-  --       dayString
-  --       [ bench "part1" $ whnf part1 inp,
-  --         bench "part2" $ whnf part2 inp
-  --       ]
-  --   ]
+  [a, b] <- map read . lines <$> readFile dayFilename :: IO [Int]
+  let inp = (a,b)
+  let inp'@(cls, dls) = part1 inp
+  print (part1 inp)
+  print (part2 (inp,inp'))
+  defaultMain
+    [ bgroup
+        dayString
+        [ bench "part1" $ whnf part1 inp,
+          bench "part2" $ whnf part2 (inp,inp')
+        ]
+    ]
